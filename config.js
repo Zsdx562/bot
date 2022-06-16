ed8918a8543c927a0ec3b68a112af47b9b88872e
Zsdx562 <107635565+Zsdx562@users.noreply.github.com>
2022-06-16 22:36:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some h
 global.thumbt = global.img
 global.fsx = 999999999999
 global.kontak = [
-['62895336282144', '𝚃𝚑𝚎.𝚂𝚊𝚍.𝙱𝚘𝚢𝟶𝟷', true],
-['6282140373458', 'Raxcel-MD ⁴⁰⁴-ﾉ [BOT MD]', true]
+['6288990007937', 'BILEK [OWNER]', true],
+['6282141404209', 'AHMAD', true]
 ]
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -34,23 +34,23 @@ global.APIKeys = { // APIKey Here
   'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
 
-global.WM = '𝑴𝒚𝑩𝒐𝒕-𝑴𝒖𝒍𝒕𝒊 𝑫𝒆𝒗𝒊𝒄𝒆 𝑩𝒚 𝚃𝚑𝚎.𝚂𝚊𝚍.𝙱𝚘𝚢𝟶𝟷'
-global.linkgc = 'https://chat.whatsapp.com/CnQV9lNcHuIENb8SQ9CaYP'
-global.linkig = 'https://www.instagram.com/the.sad.boy01'
+global.WM = 'AHMAD'
+global.linkgc = 'https://chat.whatsapp.com/D4zBlu4eqSpIyj0b6ipKsM'
+global.linkig = '-'
 global.deslink = ''
-global.titlink = 'Folllow me on Instagram'
-global.bodlink = '@the.sad.boy01'
+global.titlink = 'Folllow me on tiktok'
+global.bodlink = '@zxc_org'
 
-global.wait = '_*𝐬𝐞𝐝𝐚𝐧𝐠 𝐝𝐢 𝐩𝐫𝐨𝐬𝐞𝐬 ...*_'
-global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
+global.wait = '_*Loding coy...*_'
+global.eror = '_*ANCRIT ERROR.*_'
 
 //========Url Template Buttons==========//
-global.dtu = '𝑭𝒐𝒍𝒍𝒐𝒘'
-global.urlnya = "https://www.instagram.com/the.sad.boy01"
+global.dtu = 'TIKTOK'
+global.urlnya = "https://www.tiktok.com/@zxc_org"
 
 //============= callButtons ============//
-global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '+62 895-3362-82144'
+global.dtc = 'OWNER'
+global.phn = '+62 889-9000-7937'
 
 //========== Tampilan Bot =============//
 global.sa = '╭─'
@@ -74,9 +74,9 @@ global.thumbnailUrl = [
 //============= Games ================//
 global.benar = '_*Benar✅*_'
 global.salah = '_*Salah❌*_'
-global.dikit = "dikit lagi, semangat ya :')"
+global.dikit = "KURANG DIKIT COI, semangat ya :')"
 
-global.multiplier = 100 // The higher, The harder levelup
+global.multiplier = 70 // The higher, The harder levelup
 
 //=========== Requirements ==========//
 
